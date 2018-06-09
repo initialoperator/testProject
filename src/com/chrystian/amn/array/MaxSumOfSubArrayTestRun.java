e5d6c5db236cbb352d00c6569056dbb9967b2a3e
Yang Zhang <yangzhang@Yangs-MacBook-Pro.local>
2018-06-09 11:44:03 -0400

    finish Kadane's algorithm

--- src/com/chrystian/amn/array/MaxSumOfSubArrayTestRun.java
+++ src/com/chrystian/amn/array/MaxSumOfSubArrayTestRun.java
@@ -11,6 +11,7 @@ If the array contains all non-negative numbers, then the problem is trivial and 
 An empty set[clarification needed] is not valid.
 There can be multiple different sub-arrays that achieve the same maximum sum to the problem.
 
+key idea about Kadane's algorithm is: if the max sum subarray lies in position k when the boundary is in i(initial) and n(end), what is the position of max sum subarray when the pointer is in n+1
 * */
     public static void main(String[] args) {
         /*use scanner: the first line of integer indicates how many test cases there are
@@ -28,13 +29,35 @@ There can be multiple different sub-arrays that achieve the same maximum sum to 
             scanner.nextLine();
             String arrayLine = scanner.nextLine();//assuming each element from the input will be a number
             String[] array = arrayLine.split("\\s");
-            Integer[] numberArray = new Integer[length];
+            int[] numberArray = new int[length];
             for(int k = 0; k < length; k++){
                 numberArray[k] = new Integer(array[k]);
-                System.out.println(array[k]);
             }
+            System.out.println(findMaxSubarray(numberArray));
+        }
+    }
 
+    public static int findMaxSubarray(int[] array){
+        int max = 0;
+        for(int i = 0; i < array.length; i++){
+            int localMax = array[i];
+             localMax = findMaxSubarray(array, i, i, localMax, localMax);
+            if(localMax > max)
+                max = localMax;
+        }
+        return max;
+    }
 
+    public static int findMaxSubarray(int[] array, int indexMax, int indexEnd, int currentMax, int currentSum){
+        if(array == null || array.length == 0)
+            return 0;
+        if(array.length - indexEnd == 1)
+            return currentMax;
+        int newSum = currentSum + array[indexEnd+1];
+        if (newSum > currentMax){
+            return findMaxSubarray(array, indexEnd+1, indexEnd+1, newSum, newSum);
+        }else{
+            return findMaxSubarray(array, indexMax, indexEnd+1, currentMax, newSum);
         }
     }
 
