c39c04bcff0b8782a8cd0137ddc9fbfa2910ed3b
Yang Zhang <yangzhang@Yangs-MacBook-Pro.local>
2018-06-09 20:07:38 -0400

    finish the listnode reverse digit integer problem

--- src/com/chrystian/amn/recursion/linkedList/ListNode.java
+++ src/com/chrystian/amn/recursion/linkedList/ListNode.java
@@ -9,9 +9,37 @@ public class ListNode {//this class is define by the problem, note that the inte
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-
-        return null;
+        //assuming val is no greater than 9 and no smaller than 0 because it's a digit
+        if (l1 == null && l2 == null) {
+            return null;
+        } else if (l1 != null && l2 == null) {
+            return l1;
+        } else if (l1 == null && l2 != null) {
+            return l2;
+        } else {
+            int sum = l1.val + l2.val;
+            int extra = 0;
+//            ListNode node = new ListNode(sum);
+            if (sum > 9) {
+                ListNode node = new ListNode(sum - 10);
+                if (l1.next == null) {
+                    ListNode nextNode = new ListNode(1);
+                    node.next = addTwoNumbers(nextNode, l2.next);
+                } else if (l2.next == null) {
+                    ListNode nextNode = new ListNode(1);
+                    node.next = addTwoNumbers(l1.next, nextNode);
+                } else {
+                    ListNode nextNode = new ListNode(1 + l1.next.val);
+                    nextNode.next = l1.next.next;
+                    node.next = addTwoNumbers(nextNode, l2.next);
+                }
+                return node;
+            } else {
+                ListNode node = new ListNode(sum);
+                node.next = addTwoNumbers(l1.next, l2.next);
+                return node;
+            }
+        }
     }
 
     public int getVal() {
